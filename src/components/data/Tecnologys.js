79802a3cc325aa021779b2f18252d84f3964e094
Brian Rico <brian.rico378@gmail.com>
2024-02-25 16:06:40 -0600

    add section about tecnologys

---
+++ src/components/data/Tecnologys.js
@@ -0,0 +1,14 @@
+
+export const tecnologys = [
+    './Tecnologys/html.png',
+    './Tecnologys/css-3.png',
+    './Tecnologys/js.png',
+    './Tecnologys/typescript.png',
+    './Tecnologys/science.png',
+    './Tecnologys/node-js.png',
+    './Tecnologys/python.png',
+    './Tecnologys/aws.png',
+    './Tecnologys/azure.png',
+    './Tecnologys/docker.png',
+    './Tecnologys/mysql.png',
+];
