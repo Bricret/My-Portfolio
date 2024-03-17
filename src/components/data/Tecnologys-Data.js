e24297f24131a908e0023b6b68fac97817899253
Brian Rico <brian.rico378@gmail.com>
2024-03-16 21:31:30 -0600

    Delete variable Tecnologys and add new arr with objects tecnologys

---
+++ src/components/data/Tecnologys-Data.js
@@ -0,0 +1,131 @@
+
+
+export const FrontendTecnologys = [
+    {
+        Icon: '/TecnologySVG/HTML.svg',
+        Title: 'HTML'
+    },
+    {
+        Icon: '/TecnologySVG/CSS.svg',
+        Title: 'CSS'
+    },
+    {
+        Icon: '/TecnologySVG/TailwindCSS.svg',
+        Title: 'Tailwind CSS'
+    },
+    {
+        Icon: '/TecnologySVG/JavaScript.svg',
+        Title: 'JavaScript'
+    },
+    {
+        Icon: '/TecnologySVG/TypeScript.svg',
+        Title: 'TypeScript'
+    },
+    {
+        Icon: '/TecnologySVG/React.svg',
+        Title: 'React'
+    },
+    {
+        Icon: '/TecnologySVG/Angular.svg',
+        Title: 'Angular'
+    },
+    {
+        Icon: '/TecnologySVG/Next.svg',
+        Title: 'Next.js'
+    },
+    {
+        Icon: '/TecnologySVG/Flutter.svg',
+        Title: 'Flutter'
+    },
+]
+
+export const ToolsTecnologys = [
+    {
+        Icon: '/TecnologySVG/Git.svg',
+        Title: 'Git'
+    },
+    {
+        Icon: '/TecnologySVG/GitHub.svg',
+        Title: 'GitHub'
+    },
+    {
+        Icon: '/TecnologySVG/GitLab.svg',
+        Title: 'GitLab'
+    },
+    {
+        Icon: '/TecnologySVG/Docker.svg',
+        Title: 'Docker'
+    },
+    {
+        Icon: '/TecnologySVG/WordPress.svg',
+        Title: 'WordPress'
+    },
+    {
+        Icon: '/TecnologySVG/Jest.svg',
+        Title: 'Jest'
+    },
+    {
+        Icon: '/TecnologySVG/FireBase.svg',
+        Title: 'FireBase'
+    },
+    {
+        Icon: '/TecnologySVG/CloudFlare.svg',
+        Title: 'Cloudflare'
+    },
+    {
+        Icon: '/TecnologySVG/Linux.svg',
+        Title: 'Linux'
+    },
+]
+export const BackendTecnologys = [
+    {
+        Icon: '/TecnologySVG/Node.svg',
+        Title: 'Node.js'
+    },
+    {
+        Icon: '/TecnologySVG/Php.svg',
+        Title: 'Php'
+    },
+    {
+        Icon: '/TecnologySVG/Python.svg',
+        Title: 'Python'
+    },
+    {
+        Icon: '/TecnologySVG/Nest.svg',
+        Title: 'NestJS'
+    },
+    {
+        Icon: '/TecnologySVG/Express.svg',
+        Title: 'Express.js'
+    },
+    {
+        Icon: '/TecnologySVG/MySql.svg',
+        Title: 'MySql'
+    },
+    {
+        Icon: '/TecnologySVG/PostgreSQL.svg',
+        Title: 'PostgreSQL'
+    },
+    {
+        Icon: '/TecnologySVG/MongoDB.svg',
+        Title: 'MongoDB'
+    },
+    {
+        Icon: '/TecnologySVG/Dart.svg',
+        Title: 'Dart'
+    },
+]
+export const LearningTecnologys = [
+    {
+        Icon: '/TecnologySVG/AWS.svg',
+        Title: 'AWS'
+    },
+    {
+        Icon: '/TecnologySVG/Astro.svg',
+        Title: 'Astro'
+    },
+    {
+        Icon: '/TecnologySVG/Azure.svg',
+        Title: 'Azure'
+    },
+]
