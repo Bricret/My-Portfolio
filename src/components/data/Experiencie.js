70aeb5a9d1b70bd2d68374c487bad01f2352e36d
Brian Rico <114421216+Bricret@users.noreply.github.com>
2024-07-18 10:49:19 -0600

    chore: Update experience data with latest information

--- src/components/data/Experiencie.js
+++ src/components/data/Experiencie.js
@@ -1,29 +1,29 @@
 export const experiencieEs = [
     {
-        title: 'Freelance: Sistema Integral de Inventario y Automatización de Caja',
-        date: 'Realizado en Enero del 2024',
-        description: 'Responsable de desarrollar un sistema de inventario y automatización de caja para la cafetería Sakura Coffee Shop. Enfocado en la automatización de la caja, el sistema permite a los empleados de la cafetería realizar ventas de manera más rápida y eficiente, mientras que permite a los administradores de la cafetería llevar un control de las ventas realizadas y el inventario de la cafetería.',
+        title: 'Desarrollador Full Stack - Leon 2000',
+        date: 'De Mayo 2022 a Julio 2024',
+        description: 'Desarrollé una plataforma integral para la gestión de documentos, tareas y notificaciones a clientes. Mejoré la eficiencia en la entrega y actualización de tareas con WebSockets, reduciendo errores y tiempos en un 40%. También optimicé el rendimiento del sistema web de León 2000, creando dashboards y reportes. Utilicé buckets S3 de AWS y almacenamiento local para documentos, e implementé GitHub para el desarrollo web de la empresa y futuros proyectos.',
         latest: true,
     },
     {
-        title: 'Proyecto Universitario: Sistema de Repositorio de Tesis de la UNAN-LEON',
-        date: 'Realizado en Septiembre del 2023',
-        description: 'Desarrollé un sistema de repositorio de tesis para la UNAN-León, el cual facilita a los estudiantes de la universidad buscar y descargar tesis de manera más rápida y eficiente, mientras que permite a los profesores de la universidad subir tesis de forma ágil y eficaz.',
+        title: 'Freelance: Sistema Integral de Inventario y Automatización de Caja',
+        date: 'Realizado en Enero del 2024',
+        description: 'Responsable de desarrollar un sistema de inventario y automatización de caja para la cafetería Sakura Coffee Shop. Enfocado en la automatización de la caja, el sistema permite a los empleados de la cafetería realizar ventas de manera más rápida y eficiente, mientras que permite a los administradores de la cafetería llevar un control de las ventas realizadas y el inventario de la cafetería.',
         latest: false,
     }
 ]
 
 export const experiencieEn = [
     {
-        title: 'Freelance: Comprehensive Inventory System and Cash Automation',
-        date: 'Done in January 2024',
-        description: 'Responsible for developing an inventory and cash automation system for the Sakura Coffee Shop cafeteria. Focused on cash automation, the system allows cafeteria employees to make sales faster and more efficiently, while allowing cafeteria administrators to keep track of sales made and cafeteria inventory.',
+        title: 'Full Stack Developer - Leon 2000',
+        date: 'From May 2022 to July 2024',
+        description: 'I developed a comprehensive platform for managing documents, tasks, and notifications to clients. I improved the efficiency in the delivery and updating of tasks with WebSockets, reducing errors and times by 40%. I also optimized the performance of the León 2000 web system, creating dashboards and reports. I used AWS S3 buckets and local storage for documents, and implemented GitHub for the company\'s web development and future projects.',
         latest: true,
     },
     {
-        title: 'University Project: UNAN-LEON Thesis Repository System',
-        date: 'Done in September 2023',
-        description: 'I developed a thesis repository system for UNAN-León, which facilitates university students to search and download theses faster and more efficiently, while allowing university professors to upload theses quickly and effectively.',
+        title: 'Freelance: Comprehensive Inventory System and Cash Automation',
+        date: 'Done in January 2024',
+        description: 'Responsible for developing an inventory and cash automation system for the Sakura Coffee Shop cafeteria. Focused on cash automation, the system allows cafeteria employees to make sales faster and more efficiently, while allowing cafeteria administrators to keep track of sales made and cafeteria inventory.',
         latest: false,
     }
 ]
